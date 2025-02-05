c646ab449994780a0fce833b5432aa076b722b46
A. Claus <alex.claus@georgebrown.ca>
2025-02-05 17:04:38 -0500

    including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Student ID: 101493318</h1>
+      <h1>Student ID: Alex Claus</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developed By: Alex Claus
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
